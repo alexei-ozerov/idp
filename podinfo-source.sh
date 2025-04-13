129c330a29bfc5cc5dabec22abccc9731ed8a8e1
Alexei Ozerov <aozerov.dev@gmail.com>
2025-04-13 16:03:24 -0400

    Remove old scripts

--- podinfo-source.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-flux create source git podinfo \
-  --url=https://github.com/stefanprodan/podinfo \
-  --branch=master \
-  --interval=1m \
-  --export >./fluxcd/clusters/development/podinfo-source.yaml
